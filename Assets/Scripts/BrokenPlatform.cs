f1fd5002b17dacf528b20960e746712a97eaf16a
Samuel Salo <samuelsalo@kamk.fi>
2021-12-11 03:57:35 +0200

    final commit

--- Assets/Scripts/BrokenPlatform.cs
+++ Assets/Scripts/BrokenPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BrokenPillar : MonoBehaviour
+public class BrokenPlatform : MonoBehaviour
 {
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -10,7 +10,7 @@ public class BrokenPillar : MonoBehaviour
         if (collision.transform.CompareTag("Player"))
         {
             GetComponent<Animator>().SetTrigger("Break");
-            Destroy(gameObject, 0.33f);
+            Destroy(gameObject, 0.3f);
         }
     }
 }
